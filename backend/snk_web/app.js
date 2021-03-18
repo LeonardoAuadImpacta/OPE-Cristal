b916dd6c0b0910f9efd82e340fb69a3a5ce1ffaa
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2021-03-18 01:17:17 -0300

    Adiciona cors

--- backend/snk_web/app.js
+++ backend/snk_web/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 app.use("/api/v1", apiRouter);
 
