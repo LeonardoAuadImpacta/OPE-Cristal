c2b0c23d75a6aa480bc3c17fa6811a4ce1b97907
Kaio Saium Teoi <15633941+kaioteoi@users.noreply.github.com>
2021-03-24 13:11:43 -0300

    Atualiza store do frontend para usar localStorage (#22)

--- frontend/snk_web/src/store/index.js
+++ frontend/snk_web/src/store/index.js
@@ -1,59 +1,68 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
     session: null,
     carrinho: []
   },
   mutations: {
+    initialiseStore(state) {
+      if (localStorage.getItem("store")) {
+        this.replaceState(
+          Object.assign(state, JSON.parse(localStorage.getItem("store")))
+        );
+      }
+    },
     setUserSession(state, payload) {
-      state.session = payload
+      state.session = payload;
     },
-
     logout(state) {
-      state.session =null
+      state.session = null;
     },
-    adicionarItem(state,item) {
-      let isNew = true
-      for(var i in state.carrinho) { 
-        let id = state.carrinho[i].item.id                   
-        if(id == item.id) {
-          state.carrinho[i].qtd = state.carrinho[i].qtd + 1 
-          isNew = false
-          break
+    adicionarItem(state, item) {
+      let isNew = true;
+      for (var i in state.carrinho) {
+        let id = state.carrinho[i].item.id;
+        if (id == item.id) {
+          state.carrinho[i].qtd = state.carrinho[i].qtd + 1;
+          isNew = false;
+          break;
         }
       }
-      if(isNew) {
-        let itemCarrinho =  {
+      if (isNew) {
+        let itemCarrinho = {
           item: item,
           qtd: 1
-        }
-        state.carrinho.push(itemCarrinho)
+        };
+        state.carrinho.push(itemCarrinho);
       }
     },
-    verifuQtdCarrinho(state){
-      let removeZero = []
-      for(var i in state.carrinho) { 
-        let qtd = state.carrinho[i].qtd                   
-        if(qtd == 0) {  
-          removeZero.push(i)
+    verifuQtdCarrinho(state) {
+      let removeZero = [];
+      for (var i in state.carrinho) {
+        let qtd = state.carrinho[i].qtd;
+        if (qtd == 0) {
+          removeZero.push(i);
         }
       }
 
-      removeZero.forEach( rm => {
+      removeZero.forEach(rm => {
         state.carrinho.splice(rm, 1);
       });
     },
-    fecharPedito(state){
-      state.carrinho = []
+    fecharPedito(state) {
+      state.carrinho = [];
     }
   },
-  actions: {
+  actions: {},
+  modules: {},
+});
 
-  },
-  modules: {
-  }
-})
+store.subscribe((mutation, state) => {
+  localStorage.setItem("store", JSON.stringify(state));
+});
+
+export default store;
