e1c6590424710b7f9c27b01217e920b27f635443
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2021-04-01 00:35:47 -0300

    [backend] Atualiza API de item carrinho com incrementOrCreate, decrementOrDelete

--- backend/snk_web/controllers/item_carrinho.js
+++ backend/snk_web/controllers/item_carrinho.js
@@ -1,26 +1,129 @@
+const CarrinhoModel = require("../models/Carrinho");
+const ProdutoModel = require("../models/Produto");
 const ItemCarrinhoModel = require("../models/ItemCarrinho");
 
-const create = (req, res, next) => {
-    return ItemCarrinhoModel.create(req.body)
-      .then(() => {
-          res.status(201).json();
+const incrementOrCreate = async (req, res, next) => {
+  try {
+    const carrinho = await CarrinhoModel.findOne({
+      where: {
+        id: req.params.idCarrinho,
+      },
+    });
+    if (!carrinho)
+      return res.status(400).json({ error: "Carrinho não encontrado" });
+
+    const produto = await ProdutoModel.findOne({
+      where: {
+        id: req.params.idProduto,
+      },
+    });
+    if (!produto)
+      return res.status(400).json({ error: "Produto não encontrado" });
+
+    const { idCarrinho, idProduto, quantidade } = req.params;
+    const precoVenda = produto.precoAtual;
+
+    return ItemCarrinhoModel.findOrCreate({
+      where: {
+        idCarrinho,
+        idProduto,
+      },
+      defaults: {
+        quantidade,
+        precoVenda,
+      },
+    })
+      .then(([item_carrinho, created]) => {
+        if (created) {
+          res.status(201).json(item_carrinho);
+        } else {
+          item_carrinho.increment("quantidade", { by: quantidade });
+          item_carrinho.reload().then((item_carrinho) => {
+            res.status(200).json(item_carrinho);
+          });
+        }
       })
       .catch((reason) => {
-          res.status(400).json();
+        console.log(reason);
+        res.status(400).json({});
       });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json();
+  }
+};
+
+const decrementOrDelete = async (req, res, next) => {
+  try {
+    const carrinho = await CarrinhoModel.findOne({
+      where: {
+        id: req.params.idCarrinho,
+      },
+    });
+    if (!carrinho)
+      return res.status(400).json({ error: "Carrinho não encontrado" });
+
+    const produto = await ProdutoModel.findOne({
+      where: {
+        id: req.params.idProduto,
+      },
+    });
+    if (!produto)
+      return res.status(400).json({ error: "Produto não encontrado" });
+
+    const { idCarrinho, idProduto, quantidade } = req.params;
+
+    return ItemCarrinhoModel.findOne({
+      where: {
+        idCarrinho,
+        idProduto,
+      },
+    })
+      .then((item_carrinho) => {
+        if (item_carrinho.quantidade <= 1) {
+          item_carrinho.destroy();
+          return res.status(200).json({});
+        } else {
+          item_carrinho.decrement("quantidade", { by: quantidade });
+          item_carrinho.reload().then((item_carrinho) => {
+            res.status(200).json(item_carrinho);
+          });
+        }
+      })
+      .catch((reason) => {
+        console.log(reason);
+        res.status(400).json({});
+      });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json();
+  }
+};
+
+const list = async (req, res, next) => {
+  const item_carrinhos = await ItemCarrinhoModel.findAll({
+    where: {
+      idCarrinho: req.query.idCarrinho,
+    },
+    include: ProdutoModel,
+  });
+
+  return res.status(200).json(item_carrinhos);
 };
 
 const get = async (req, res, next) => {
-    const item_carrinho = await ItemCarrinhoModel.findOne({
-        where: {
-            id: req.params.id,
-        },
-    });
+  const item_carrinho = await ItemCarrinhoModel.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
 
-    return res.status(200).json(item_carrinho);
+  return res.status(200).json(item_carrinho);
 };
 
 module.exports = {
-    create,
-    get,
+  incrementOrCreate,
+  decrementOrDelete,
+  list,
+  get,
 };
--- backend/snk_web/routes/api/item_carrinho.js
+++ backend/snk_web/routes/api/item_carrinho.js
@@ -6,52 +6,138 @@ const itemCarrinhoController = require("../../controllers/item_carrinho");
 
 const router = express.Router();
 
-const createItemCarrinhoSchema = {
-    idCarrinho: {
-        in: ["body"],
-        errorMessage: "Id do carrinho inválido",
-        isEmpty: { negated: true },
-        isInt: true,
+const incrementOrCreateItemCarrinhoSchema = {
+  idCarrinho: {
+    in: ["params"],
+    errorMessage: "Id do carrinho inválido",
+    isEmpty: {
+      negated: true,
+      errorMessage: "Id do carrinho é obrigatório",
     },
-    idProduto: {
-        in: ["body"],
-        errorMessage: "Id do produto inválido",
-        isEmpty: { negated: true },
-        isInt: true,
+    isInt: {
+      errorMessage: "Id do carrinho deve ser numérico",
     },
-    quantidade: {
-        in: ["body"],
-        errorMessage: "Quantidade inválida",
-        isEmpty: { negated: true },
-        isInt: true,
+  },
+  idProduto: {
+    in: ["body"],
+    errorMessage: "Id do produto inválido",
+    isEmpty: {
+      negated: true,
+      errorMessage: "Id do produto é obrigatório",
     },
-    precoVenda: {
-        in: ["body"],
-        errorMessage: "Preço inválido",
-        isEmpty: { negated: true },
-        isDecimal: true,
+    isInt: {
+      errorMessage: "Id do produto deve ser numérico",
     },
+  },
+  quantidade: {
+    in: ["body"],
+    errorMessage: "Quantidade inválida",
+    isEmpty: {
+      negated: true,
+      errorMessage: "Quantidade é um campo obrigatório",
+    },
+    toInt: {
+      errorMessage: "Quantidade deve ser numérico",
+    },
+  },
+  // precoVenda será calculado direto no controller
+  //precoVenda: {
+  //in: ["body"],
+  //errorMessage: "Preço inválido",
+  //isEmpty: {
+  //negated: true,
+  //errorMessage: "Preço é obrigatório"
+  //},
+  //isDecimal: true,
+  //},
 };
 
 router.post(
-    "/",
-    validate([checkSchema(createItemCarrinhoSchema)]),
-    itemCarrinhoController.create
+  "/carrinho/:idCarrinho",
+  validate([checkSchema(incrementOrCreateItemCarrinhoSchema)]),
+  itemCarrinhoController.incrementOrCreate
 );
 
-const getItemCarrinhoSchema = {
-    id: {
-        in: ["params"],
-        errorMessage: "id do item do pedido inválido",
-        isInt: true,
-        toInt: true,
+const decrementOrDeleteItemCarrinhoSchema = {
+  idCarrinho: {
+    in: ["params"],
+    errorMessage: "Id do carrinho inválido",
+    isEmpty: {
+      negated: true,
+      errorMessage: "Id do carrinho é obrigatório",
     },
+    isInt: {
+      errorMessage: "Id do carrinho deve ser numérico",
+    },
+  },
+  idProduto: {
+    in: ["body"],
+    errorMessage: "Id do produto inválido",
+    isEmpty: {
+      negated: true,
+      errorMessage: "Id do produto é obrigatório",
+    },
+    isInt: {
+      errorMessage: "Id do produto deve ser numérico",
+    },
+  },
+  quantidade: {
+    in: ["body"],
+    errorMessage: "Quantidade inválida",
+    isEmpty: {
+      negated: true,
+      errorMessage: "Quantidade é um campo obrigatório",
+    },
+    toInt: {
+      errorMessage: "Quantidade deve ser numérico",
+    },
+  },
+};
+
+router.delete(
+  "/carrinho/:idCarrinho",
+  validate([checkSchema(decrementOrDeleteItemCarrinhoSchema)]),
+  itemCarrinhoController.decrementOrDelete
+);
+
+const listItemCarrinhoSchema = {
+  idCarrinho: {
+    in: ["query"],
+    isInt: {
+      errorMessage: "Id do carrinho deve ser numérico",
+    },
+  },
+  _pagina: {
+    in: ["query"],
+    errorMessage: "Número da página inválido",
+    toInt: true,
+  },
+  _items: {
+    in: ["query"],
+    errorMessage: "Limite de items por página inválido",
+    toInt: true,
+  },
 };
 
 router.get(
-    "/:id",
-    validate([checkSchema(getItemCarrinhoSchema)]),
-    itemCarrinhoController.get
+  "/",
+  validate([checkSchema(listItemCarrinhoSchema)]),
+  itemCarrinhoController.list
+);
+
+const getItemCarrinhoSchema = {
+  id: {
+    in: ["params"],
+    errorMessage: "id do item do pedido inválido",
+    isInt: true,
+    toInt: true,
+  },
+};
+
+router.get(
+  "/:id",
+  validate([checkSchema(getItemCarrinhoSchema)]),
+  itemCarrinhoController.get
 );
 
 module.exports = router;
