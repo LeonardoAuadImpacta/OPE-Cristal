06d166cf05cbf852e3d6677035b3f5b10ecfa9f0
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2021-04-01 00:52:12 -0300

    [frontend] Integra components de carrinho com serviço de itens do carrinho

--- frontend/snk_web/src/components/SnkCard.vue
+++ frontend/snk_web/src/components/SnkCard.vue
@@ -1,31 +1,28 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="350px"
-  >
-    <v-img
-      :src="card.url"
-      height="200px"
-    ></v-img>
+  <v-card class="mx-auto" max-width="350px">
+    <v-img :src="card.url" height="200px"></v-img>
 
     <v-card-title>
-      {{card.titulo}}
+      {{ card.nome }}
     </v-card-title>
 
     <v-card-subtitle>
-      {{card.sub_titulo}}
+      {{ card.subtitulo }}
     </v-card-subtitle>
-    
+
     <div class="snk-flex-around">
       <label>
-        {{formatPrice(card.preco)}}
+        {{ formatPrice(card.precoAtual) }}
       </label>
-      <v-btn @click=adicionar(card) class="snk-qtd" color="#aa2514">
+      <v-btn @click="adicionar(card)" class="snk-qtd" color="#aa2514">
         <div class="snk-alert">
-          <v-alert :value ="carPlus" outlined color="transparent" transition="scroll-y-reverse-transition">
-            <v-icon color="#aa2514">
-              mdi-cart-plus
-            </v-icon>
+          <v-alert
+            :value="carPlus"
+            outlined
+            color="transparent"
+            transition="scroll-y-reverse-transition"
+          >
+            <v-icon color="#aa2514"> mdi-cart-plus </v-icon>
           </v-alert>
         </div>
         Adicionar
@@ -33,17 +30,12 @@
     </div>
 
     <v-card-actions>
-      <v-btn color="orange lighten-2" text >
-        Detalhes
-      </v-btn>
+      <v-btn color="orange lighten-2" text> Detalhes </v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      <v-btn icon @click="show = !show">
+        <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
       </v-btn>
     </v-card-actions>
 
@@ -52,59 +44,61 @@
         <v-divider></v-divider>
 
         <v-card-text>
-          {{card.desc}}
+          {{ card.descricao }}
         </v-card-text>
       </div>
     </v-expand-transition>
   </v-card>
 </template>
 
-
 <script>
-  export default {
-    name: 'SnkCard',
-    props: {
-        card: {
-            id: Number,
-            url: String,
-            titulo: String,
-            sub_titulo: String,
-            preco: Number,
-            desc: String
-        }
+
+export default {
+  name: "SnkCard",
+  props: {
+    card: {
+      id: Number,
+      nome: String,
+      subtitulo: String,
+      descricao: String,
+      precoAtual: Number,
+      url: String,
     },
-    data: () => ({
-      show: false,
-      carPlus: false
-    }),
-    methods:{
-         formatPrice(value) {
-            let val = (value/1).toFixed(2).replace('.', ',')
-            return "R$ ".concat(val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."))
-        },
-        adicionar(card) {
-          this.carPlus= true
-          setTimeout(() => this.carPlus=false, 500)
-          this.$store.commit("adicionarItem",card)
-        }
-    }
-  }
+  },
+  data: () => ({
+    show: false,
+    carPlus: false,
+  }),
+  methods: {
+    formatPrice(value) {
+      let val = (value / 1).toFixed(2).replace(".", ",");
+      return "R$ ".concat(val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."));
+    },
+    adicionar(card) {
+      console.log(card);
+      this.carPlus = true;
+      setTimeout(() => (this.carPlus = false), 500);
+      this.$store.dispatch("adicionarItem", {
+        idCliente: this.$store.state.session.id,
+        idProduto: card.id
+      });
+    },
+  },
+};
 </script>
 
-
 <style scoped>
-    .snk-qtd {
-        color: white;
-    }
-    .snk-flex-around label {
-        max-width: 30%;
-        vertical-align: middle;
-        font-weight: bold;
-        padding-top: 7px;
-    }
-    .snk-alert{
-      position: absolute;
-      top: -300%;
-    }
-
+.snk-qtd {
+  color: white;
+}
+.snk-flex-around label {
+  max-width: 30%;
+  vertical-align: middle;
+  font-weight: bold;
+  padding-top: 7px;
+}
+.snk-alert {
+  position: absolute;
+  top: -300%;
+}
 </style>
