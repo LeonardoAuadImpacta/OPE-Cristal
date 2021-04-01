f4223e6c766757110339d62275eb1dc528cd12fb
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2021-04-01 00:38:56 -0300

    [backend] Corrige formatação nos arquivos api/endereco.js e api/pedido.js

--- backend/snk_web/routes/api/pedido.js
+++ backend/snk_web/routes/api/pedido.js
@@ -7,47 +7,56 @@ const pedidoController = require("../../controllers/pedido");
 const router = express.Router();
 
 const createPedidoSchema = {
-    idCliente: {
-        in: ["body"],
-        errorMessage: "Id do cliente inválido",
-        isEmpty: { negated: true },
-        isInt: true,
+  idCliente: {
+    in: ["body"],
+    errorMessage: "Id do cliente inválido",
+    isEmpty: { negated: true },
+    isInt: true,
+  },
+  idCarrinho: {
+    in: ["body"],
+    errorMessage: "Id do carrinho inválido",
+    isEmpty: { negated: true },
+    isInt: true,
+  },
+  status: {
+    in: ["body"],
+    errorMessage: "Status inválido",
+    isEmpty: { negated: true },
+    isIn: {
+      options: [
+        [
+          "COMPLETED",
+          "CANCELED",
+          "AWAITING_PAYMENT",
+          "CONFIRMED",
+          "DISPATCHED",
+          "IN_TRANSIT",
+        ],
+      ],
     },
-    idCarrinho: {
-        in: ["body"],
-        errorMessage: "Id do carrinho inválido",
-        isEmpty: { negated: true },
-        isInt: true,
-    },
-    status: {
-        in: ["body"],
-        errorMessage: "Status inválido",
-        isEmpty: { negated: true },
-        isIn: {
-            options: [["COMPLETED", "CANCELED", "AWAITING_PAYMENT", "CONFIRMED", "DISPATCHED", "IN_TRANSIT"]]
-        },
-    },
+  },
 };
 
 router.post(
-    "/",
-    validate([checkSchema(createPedidoSchema)]),
-    pedidoController.create
+  "/",
+  validate([checkSchema(createPedidoSchema)]),
+  pedidoController.create
 );
 
 const getPedidoSchema = {
-    id: {
-        in: ["params"],
-        errorMessage: "Id do pedido inválido",
-        isInt: true,
-        toInt: true,
-    },
+  id: {
+    in: ["params"],
+    errorMessage: "Id do pedido inválido",
+    isInt: true,
+    toInt: true,
+  },
 };
 
 router.get(
-    "/:id",
-    validate([checkSchema(getPedidoSchema)]),
-    pedidoController.get
+  "/:id",
+  validate([checkSchema(getPedidoSchema)]),
+  pedidoController.get
 );
 
 module.exports = router;
