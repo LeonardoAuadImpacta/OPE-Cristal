78adb835a680453e9322b4c953693d3cab304b6c
kaioteoi <15633941+kaioteoi@users.noreply.github.com>
2021-04-13 21:02:17 -0300

    Resolve conflito no arquivo model/Cliente.js do backend

--- backend/snk_web/controllers/login.js
+++ backend/snk_web/controllers/login.js
@@ -1,34 +1,30 @@
 const clienteModel = require("../models/Cliente");
-module.exports =  async (req, res, next) => {
-    const token = "snk-token";
-    const resEmail = req.body.email;
-    const resSenha = req.body.senha;
+module.exports = async (req, res, next) => {
+  const token = "snk-token";
+  const resEmail = req.body.email;
+  const resSenha = req.body.senha;
 
+  const cliente = await clienteModel.findOne({
+    where: {
+      email: resEmail,
+    },
+  });
 
-    const cliente = await  clienteModel.findOne(
-        {
-            where: {
-                email: resEmail
-            }
-        }
-    )
-
-    if (cliente === null ) {
-        return res.status(401).json({
-            "message": "Usuario não cadastrado"
-        })
-    }
-
-    if (cliente.senha !== resSenha) {
-        return  res.status(403).json({
-            "message": "Email ou senha inválidos"
-        })
-    }
-    return res.json({
-        id: cliente.id,
-        token: token,
-        username: cliente.nome,
-        profile: cliente.profile,
+  if (cliente === null) {
+    return res.status(401).json({
+      message: "Usuario não cadastrado",
     });
+  }
 
-}
+  if (cliente.senha !== resSenha) {
+    return res.status(403).json({
+      message: "Email ou senha inválidos",
+    });
+  }
+  return res.json({
+    id: cliente.id,
+    token: token,
+    username: cliente.nome,
+    profile: cliente.profile,
+  });
+};
--- backend/snk_web/seeders/20210408001824-seed-cliente.js
+++ backend/snk_web/seeders/20210408001824-seed-cliente.js
@@ -1,29 +1,32 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('cliente', [{
-      nome: "User",
-      sobrenome: "Customer",
-      pseudonimo: "user_customer",
-      telefone: "1190909090",
-      email: "user.customer@email.com",
-      senha: "1234abcd",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }, {
-      nome: "User",
-      sobrenome: "Admin",
-      pseudonimo: "user_admin",
-      telefone: "1190909090",
-      email: "user.admin@email.com",
-      senha: "1234abcd",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }]);
+    await queryInterface.bulkInsert("cliente", [
+      {
+        nome: "User",
+        sobrenome: "Customer",
+        pseudonimo: "user_customer",
+        telefone: "1190909090",
+        email: "user.customer@email.com",
+        senha: "1234abcd",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        nome: "User",
+        sobrenome: "Admin",
+        pseudonimo: "user_admin",
+        telefone: "1190909090",
+        email: "user.admin@email.com",
+        senha: "1234abcd",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('cliente', null, {});
-  }
+    await queryInterface.bulkDelete("cliente", null, {});
+  },
 };
