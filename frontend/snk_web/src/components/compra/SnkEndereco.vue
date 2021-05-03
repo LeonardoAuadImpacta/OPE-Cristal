fd97fcb00ff7984f1eda95e0aed2a979487f9375
brunoVidalImpacta <bruno.vidal@aluno.faculdadeimpacta.com.br>
2021-05-03 18:43:35 -0300

    Novo endereco

--- frontend/snk_web/src/components/compra/SnkEndereco.vue
+++ frontend/snk_web/src/components/compra/SnkEndereco.vue
@@ -1,37 +1,39 @@
 <template>
-    <div class="snk-snep">
-        <v-radio-group
-              v-model="selecionado"
-              row
-            >
-        <div class="snk-flex card" v-for="(endereco,idx) in enderecos" :key="idx">
-            <v-card >
-                <v-card-title>
-                    <label>
-                        {{endereco.rua}} nº {{endereco.numero}}
-                    </label>
-                    
-                            <v-radio
-                                color="red"
-                                :value="endereco"
-                                class="snk-radio"
-                                @click="selecionarEndereco(endereco)"
-                            ></v-radio>
-                </v-card-title>
-                <v-card-subtitle>
-                    {{endereco.bairro}}
-                </v-card-subtitle>
-                <v-card-text>
-                    CEP: {{endereco.cep}}
-                </v-card-text>
-            </v-card>
-        </div>
-        </v-radio-group>    
-    </div>    
+    <div>
+        <div class="snk-snep">
+            <v-radio-group
+                v-model="selecionado"
+                row
+                >
+            <div class="snk-flex card" v-for="(endereco,idx) in enderecos" :key="idx">
+                <v-card >
+                    <v-card-title>
+                        <label>
+                            {{endereco.rua}} nº {{endereco.numero}}
+                        </label>
+                        
+                                <v-radio
+                                    color="red"
+                                    :value="endereco"
+                                    class="snk-radio"
+                                    @click="selecionarEndereco(endereco)"
+                                ></v-radio>
+                    </v-card-title>
+                    <v-card-subtitle>
+                        {{endereco.bairro}}
+                    </v-card-subtitle>
+                    <v-card-text>
+                        CEP: {{endereco.cep}}
+                    </v-card-text>
+                </v-card>
+            </div>
+            </v-radio-group>    
+        </div>   
+        <SnkCriacaoEndereco />  
+    </div>   
 </template>
 
 <script>
-//import enderecosJson from '../../assets/mock_service/EnderecoMockService.json'
 import { listEnderecoByIdCliente } from '../../controller/SnkEnderecoController.js';
 export default {
     data() {
@@ -49,6 +51,9 @@ export default {
             this.$emit("selecionarEndereco",endereco);
         }
     },
+    components: {
+        SnkCriacaoEndereco
+    }
 }
 </script>
 
@@ -62,6 +67,11 @@ export default {
     }
     .snk-radio{
         right: 0;
+    }
+    .flex-col {
+        display: flex;
+        flex-direction: column;
+        margin: 5%;
     }
 
 </style>
