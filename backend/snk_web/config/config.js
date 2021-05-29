9e903dc8d2b3d657f484f4a081158bc3c9551075
Kaio Saium Teoi <15633941+kaioteoi@users.noreply.github.com>
2021-05-29 14:53:16 -0300

    Configura execução de migrations em prod (#40)

--- backend/snk_web/config/config.js
+++ backend/snk_web/config/config.js
@@ -24,11 +24,11 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DBNAME,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
     dialect: "mysql",
   },
 };
